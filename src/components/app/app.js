8aa5949626ff5c70ce39ef5fff5012f3b6f6b915
DESKTOP-SS2MF1D\ED <kornet@greenline-software.com>
2022-01-04 22:47:58 +0300

    [CHANGED] function app to class App

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,31 +1,40 @@
-import "./app.css";
+import {Component} from 'react';
+
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
 import EmployersList from "../emlpoyers-list/employers-list";
 import EmployeesAddForm from "../employers-add-form/employers-add-form";
 
+import "./app.css";
 
-function App() {
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data : [
+                {name : "John", salary: 800, increase : false, id: 1},
+                {name : "Ed", salary: 300, increase : true, id: 2},
+                {name : "Alex", salary: 500, increase : false, id: 3},
+                {name : "Olehandro", salary: 5000, increase : false, id: 4}
+            ]
+        }
+    }
 
-    const data = [
-        {name : "John", salary: 800, increase : false, id: 1},
-        {name : "Ed", salary: 300, increase : true, id: 2},
-        {name : "Alex", salary: 500, increase : false, id: 3},
-        {name : "Olehandro", salary: 5000, increase : false, id: 4},
-    ]
-
-    return (
-        <div className="app">
-            <AppInfo/>
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+    render() {
+        return (
+            <div className="app">
+                <AppInfo/>
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+                <EmployersList data={data}
+                               onDelete={id => console.log(id)}/>
+                <EmployeesAddForm/>
             </div>
-            <EmployersList data={data}/>
-            <EmployeesAddForm/>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
--- src/components/emlpoyers-list/employers-list.js
+++ src/components/emlpoyers-list/employers-list.js
@@ -2,12 +2,14 @@
 import "./employers-list.css";
 import EmployersListItem from "../employers-list-item/employers-list-item";
 
-const EmployersList = ({data}) => {
+const EmployersList = ({data, onDelete}) => {
 
     const elements = data.map(item => {
         const {id, ...itemProps} = item;
         return (
-            <EmployersListItem key={id} {...itemProps}/>
+            <EmployersListItem key={id}
+                               {...itemProps}
+                                onDelete={() => onDelete(id)}/>
         )
     })
 
